09d2ce30abe2328cf81305f36e64a0b44aee2e3f
Kapil Thangavelu <kapilt@users.noreply.github.com>
2006-07-19 23:01:31 +0000

    break out from zope2 product into z3 style package

---
+++ ore.alchemist/trunk/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ore.alchemist",
+    version="0.1.0",
+    packages=find_packages('src', exclude=["*.tests"]),
+    package_dir= {'':'src'},
+    namespace_packages=['ore'],
+    package_data = {
+    '': ['*.txt', '*.zcml', '*.pt'],
+    },
+
+    zip_safe=False,
+    author='ObjectRealms',
+    author_email='info@objectrealms.net'
+    description="""\
+ore.alchemist contains an integration of sqlalchemy into the
+Zope App server environment. It can be used with Zope2, Zope3 or
+standalone.
+""",
+    license='GPL',
+    keywords="zope zope3",
+    )
