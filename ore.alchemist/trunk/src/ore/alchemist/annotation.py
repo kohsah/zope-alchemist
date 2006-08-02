879d957990e47e37a1073699a8c6ddf5f6f65dfa
Kapil Thangavelu <kapilt@users.noreply.github.com>
2006-08-02 00:53:14 +0000

    start prelim work on nice default container views

--- ore.alchemist/trunk/src/ore/alchemist/annotation.py
+++ ore.alchemist/trunk/src/ore/alchemist/annotation.py
@@ -7,6 +7,28 @@ to application specific column metadata.
 $Id$
 """
 
+from zope.interface import implements
+from interfaces import IModelAnnotation
+
+    
+class ModelAnnotation( object ):
+    
+    implements( IModelAnnotation )
+    
+    def __init__(self, context, annotation):
+        self.context = context
+        self.annotation = annotation
+
+    def getDisplayColumns(self):
+        from zc.table.column import GetterColumn        
+        columns = []
+        for i in self.annotation.values():
+            column = GetterColumn( title = i.title,
+                                   name = i.title,
+                                   getter = lambda ob: getattr( ob, i.title ) )
+            columns.append( column )
+
+
 class TableAnnotation( dict ):
 
     __slots__ = ("table_name",)
@@ -15,5 +37,8 @@ class TableAnnotation( dict ):
         self.table_name = table_name
         super( TableAnnotation, self).__init__( **kw )
 
+    def __call__( self, context ):
+        return ModelAnnotation( context, self )
 
+        
 class ColumnAnnotation( dict ): pass        
