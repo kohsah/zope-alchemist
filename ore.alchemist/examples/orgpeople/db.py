eae38b5332532338ff2a2cd527972ee6b86ff471
Kapil Thangavelu <kapilt@users.noreply.github.com>
2006-11-27 22:50:41 +0000

    fix db encoding issues, setup db encoding as utf8 and coresponding client encoding and unicode marshalling

--- ore.alchemist/examples/orgpeople/db.py
+++ ore.alchemist/examples/orgpeople/db.py
@@ -6,4 +6,4 @@ $Id$
 
 from ore.alchemist.engine import get_engine
 
-database = get_engine('mysql://root@localhost/orgperson', echo=True)
+database = get_engine('mysql://root@localhost/orgperson', encoding='utf-8', convert_unicode=True, echo=True)
