f90e671605a3f3bf9f16823bfcbd065e50ffffc1
Kapil Thangavelu <kapilt@users.noreply.github.com>
2006-11-28 13:07:19 +0000

    pull in ui improvements / infrastructure from orgpeople into alchemist

--- alchemist/trunk/browser/formlib.py
+++ alchemist/trunk/browser/formlib.py
@@ -2,10 +2,76 @@
 Form Lib Variants which play nice with alchemist data models
 """
 
+import pytz
+import datetime
+
+import zope.event
+import zope.app.event.objectevent
+
+from zope.interface.common import idatetime
 from zope.schema.interfaces import IObject
+from zope.component import getAdapter
 from zope.formlib.i18n import _
+from zope.formlib import form
+from zope.app.form import CustomWidgetFactory
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from Products.Five.formlib import formbase
+
 from ore.alchemist.interfaces import ITableSchema
 from ore.alchemist.manager import get_session
+
+class RecordDisplayForm( formbase.DisplayForm ):
+    template = ZopeTwoPageTemplateFile('templates/data_view.pt')
+    prefix = 'view'    
+
+class RecordEditForm( formbase.EditFormBase ):
+    template = ZopeTwoPageTemplateFile('templates/data_edit.pt')    
+    prefix = 'edit'
+
+    @form.action("Apply", condition=form.haveInputWidgets)
+    def handle_edit_action(self, action, data):
+        # we want to use a custom applyChanges which detects subobject in place
+        # edits.
+        if applyChanges(self.context, self.form_fields, data, self.adapters):
+            zope.event.notify(
+                zope.app.event.objectevent.ObjectModifiedEvent(self.context))
+            formatter = self.request.locale.dates.getFormatter(
+                'dateTime', 'medium')
+
+            try:
+                time_zone = idatetime.ITZInfo(self.request)
+            except TypeError:
+                time_zone = pytz.UTC
+
+            status = _("Updated on ${date_time}",
+                       mapping={'date_time':
+                                formatter.format(
+                                   datetime.datetime.now(time_zone)
+                                   )
+                        }
+                       )
+            self.status = status
+        else:
+            self.status = _('No changes')
+
+    def update( self, *args, **kw):
+        super( RecordEditForm, self).update( *args, **kw )
+        self.request.set('portal_status_message', self.status )    
+
+class RecordAddForm(  formbase.AddFormBase ):
+    template = ZopeTwoPageTemplateFile('templates/data_edit.pt')        
+    prefix = 'add'
+    
+    def createAndAdd(self, data):
+        record = self.context.domain_model( **data )
+        self._finished_add = True
+        return record
+
+    def nextURL( self ):
+        return self.context.absolute_url()    
+
 
 def applyChanges(context, form_fields, data, adapters=None):
     """
