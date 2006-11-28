46555c74ba194eeb65e8740be586a2ba7873583a
Kapil Thangavelu <kapilt@users.noreply.github.com>
2006-11-28 19:19:02 +0000

    document via example the new ordering options, and syntax passing in properties

--- ore.alchemist/examples/orgpeople/interfaces.py
+++ ore.alchemist/examples/orgpeople/interfaces.py
@@ -16,6 +16,23 @@ from zope import schema as zschema
 from zope.schema.interfaces import IContextSourceBinder
 
 import schema
+from vocabulary import StateVocabulary
+
+
+AddressAnnotation = TableAnnotation(
+    "Address",
+    columns = [
+        dict( name="address_id", omit = True ), # omit means that the we never see the id in the schema
+        dict( name="address_1", label = "address line 1"),
+        dict( name="address_2", label = "address line 2"),
+        ],
+    properties = {'state':zschema.Choice( title=u"state", vocabulary=StateVocabulary() ) }
+    )
+
+IAddressTable = transmute( schema.AddressTable,
+                           AddressAnnotation,                           
+                           __module__="Products.orgperson.interfaces" )
+
 
 PersonAnnotation = TableAnnotation(
     "Person",
@@ -27,29 +44,19 @@ PersonAnnotation = TableAnnotation(
          dict( name="phone_number", label = "Home Phone Number" ),
          dict( name="address_id", omit = True ),
          dict( name="person_id", omit = True )
-         ]
+         ],
+# you can specify which columns should be in the container listing view via this, first column
+# always links to record
+        table_columns = ('first_name', 'last_name', 'email', 'phone_number'), 
+# you can specify the order of the fields globally via this mechanism
+#    schema_order = ('email', 'phone_number'),
+    properties = {'address':zschema.Object( IAddressTable, required=False ) },    
     )
 
-AddressAnnotation = TableAnnotation(
-    "Address",
-    columns = [
-        dict( name="address_id", omit = True ), # omit means that the we never see the id in the schema
-        dict( name="address_1", label = "address line 1"),
-        dict( name="address_2", label = "address line 2"),
-        ]
-    )
-
-
-from vocabulary import StateVocabulary
                   
-IAddressTable = transmute( schema.AddressTable,
-                           AddressAnnotation,
-                           properties = {'state':zschema.Choice( title=u"state", vocabulary=StateVocabulary() ) },
-                           __module__="Products.orgperson.interfaces" )
 
 IPersonTable = transmute( schema.PersonTable,
                           PersonAnnotation,
-                          properties = {'address':zschema.Object( IAddressTable, required=False ) },
                           __module__="Products.orgperson.interfaces" )
 
 
