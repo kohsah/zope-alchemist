1031432d0f7f8fe816a564a02f27ab298d0ed6ab
Kapil Thangavelu <kapilt@users.noreply.github.com>
2006-11-29 18:35:27 +0000

    better locale setup during traversal

--- alchemist/trunk/traversal.py
+++ alchemist/trunk/traversal.py
@@ -26,7 +26,10 @@ $Id$
 from zope.app.traversing.interfaces import ITraversable, TraversalError
 from zope.component import ComponentLookupError
 from zope.interface import implements
-from zope.i18n.locales import locales
+
+from zope.i18n.interfaces import IUserPreferredCharsets
+from zope.i18n.interfaces import IUserPreferredLanguages
+from zope.i18n.locales import locales, LoadLocaleError
 
 from zExceptions import NotFound
 
@@ -38,10 +41,35 @@ class ContainerTraversal( FiveTraversable ):
 
     def __init__(self, context ):
         self._subject = context
-        # setup locales
-        if not getattr( context.REQUEST, 'locale', None):
-            context.REQUEST.locale = locales.getLocale( None, None, None )
 
+        # setup locales
+        self.setupLocale()
+
+    def setupLocale( self ):
+        # slightly adapted from zope.publisher.http.HTTPRequest.setupLocale
+        request = self._subject.REQUEST
+        if getattr( request, 'locale', None) is not None:
+            return
+        
+        envadapter = IUserPreferredLanguages(request, None)
+        if envadapter is None:
+            request.locale = locales.getLocale(None, None, None)            
+            return
+
+        langs = envadapter.getPreferredLanguages()
+        for httplang in langs:
+            parts = (httplang.split('-') + [None, None])[:3]
+            try:
+                request.locale = locales.getLocale(*parts)
+                return
+            except LoadLocaleError:
+                # Just try the next combination
+                pass
+        else:
+            # No combination gave us an existing locale, so use the default,
+            # which is guaranteed to exist
+            request.locale = locales.getLocale(None, None, None)
+            
     def traverse( self, name, furtherPath ):
         # first try to find a view
         try:
