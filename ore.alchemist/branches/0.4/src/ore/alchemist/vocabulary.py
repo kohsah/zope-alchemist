e9c2ea9aaec7581828d46b48208a7ba958649efd
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-01-02 16:59:09 +0000

    add new vocabulary which stores object as value

--- ore.alchemist/branches/0.4/src/ore/alchemist/vocabulary.py
+++ ore.alchemist/branches/0.4/src/ore/alchemist/vocabulary.py
@@ -1,22 +1,20 @@
 """
 
-
 $Id$
 """
 
 from ore.alchemist import Session
 from zope import interface
 from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary
-
+from zope.schema import vocabulary
 
 class DatabaseSource( object ):
     """
     a simple implementation of vocabularies on top of a domain model, ideally should
-    only be used with small skinny tables.
+    only be used with small skinny tables, actual value stored is the id
     """
     interface.implements( IContextSourceBinder )
-
+    
     def __init__( self, domain_model, token_field, value_field ):
         self.domain_model = domain_model
         self.token_field = token_field
@@ -29,23 +27,34 @@ class DatabaseSource( object ):
         keyvalues = [ (getattr( ob, self.token_field), getattr( ob, self.value_field) ) \
                       for ob in results ]
                       
-        return SimpleVocabulary.fromItems( keyvalues )
-        
+        return vocabulary.SimpleVocabulary.fromItems( keyvalues )
+
+class DatabaseObjectSource( DatabaseSource ):
+    """
+    a vocabulary source, where objects are the values, for suitable for o2m fields.
+    """
+    def __call__( self, context ):
+        session = Session()
+        query = session.query( self.domain_model )
+        results = query.all()
+        terms = [vocabulary.SimpleTerm( value=ob, token=getattr( ob, self.value_field), title=getattr( ob, self.token_field ) ) \
+                 for ob in results ]
+        return vocabulary.SimpleVocabulary( terms )
 
 class VocabularyTable( object ):
     """
-    simple implementation of vocabularies on top of an rdb table. this is a sample implementation
-    it doesn't really do well if the vocab table is being modified.
+    a database source implementation which caches values for the lifetime of the app, useful
+    if the vocabulary definition is static.
     """
-
+    
     def __init__( self, table, token_field, value_field ):
         self.table = table
         self.token_field = token_field
         self.value_field = value_field
-
+        
         terms = select( [getattr( table.c, value_field ),
                          getattr( table.c, token_field ) ] ).execute().fetchall()
-        self.vocabulary = SimpleVocabulary.fromItems( terms )
+        self.vocabulary = vocabulary.SimpleVocabulary.fromItems( terms )
         
     def __getattr__( self, name ):
         return getattr( self.vocabulary, name )
