e9f5b0e69014c74858dcf526c4ff0d1099deb97d
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-01-13 16:35:51 +0000

    fix accessor for oid links

--- alchemist.ui/trunk/alchemist/ui/container.py
+++ alchemist.ui/trunk/alchemist/ui/container.py
@@ -9,6 +9,7 @@ from zope.security import proxy
 from zc.table import column
 from zc.table import table
 
+from sqlalchemy import orm
 from ore.alchemist.model import queryModelDescriptor
 from i18n import _
 
@@ -21,48 +22,49 @@ class Getter( object ):
         return self.getter( item )
 
 def viewLink( item, formatter ):
-    return '<a class="button-link" href="%s">View</a>'%item.id
+    return '<a class="button-link" href="%s">View</a>'%(getattr( item, formatter.oid_key ))
 
 def editLink( item, formatter ):
-    return '<a class="button-link" href="%s/edit">Edit</a>'%item.id
+    return '<a class="button-link" href="%s/edit">Edit</a>'%(getattr( item, formatter.oid_key))
 
 def viewEditLinks( item, formatter ):
     return '%s %s'%(viewLink( item, formatter), editLink( item, formatter ) )
 
+
 class ContainerListing( form.DisplayForm ):
-
+    
     form_fields = form.Fields()
-
+    
     def update( self ):
         context = proxy.removeSecurityProxy( self.context )
         columns = []
-
+        
         domain_model = context.domain_model
         domain_interface = list( interface.implementedBy(domain_model) )[0]
         domain_annotation = queryModelDescriptor( domain_interface )
-
+        
         field_column_names = domain_annotation and domain_annotation.listing_columns \
                              or schema.getFieldNamesInOrder( domain_interface )
-
+        
         for field_name in field_column_names:
             field = domain_interface[ field_name ]
             columns.append(
                 column.GetterColumn( title= ( field.title or field.__name__ ),
                                      getter = Getter( field.query ) )
                 )
-
+        
         columns.append(
             column.GetterColumn( title = _(u'Actions'),
                                  getter = viewEditLinks )
             )
         
         self.columns = columns
-
+        
         super( ContainerListing, self).update()
-
+        
     def render( self ):
         return self.index()
-
+        
     def listing( self ):
         context = proxy.removeSecurityProxy( self.context )
         
@@ -72,6 +74,9 @@ class ContainerListing( form.DisplayForm ):
                                             prefix="form",
                                             visible_column_names = [c.name for c in self.columns],
                                             columns = self.columns )
+        
+        # TODO : single primary key, need to reexamine 
+        formatter.oid_key = orm.class_mapper( context.domain_model ).primary_key[0].name
         formatter.cssClasses['table'] = 'datagrid'
         formatter.table_id = "datacontents"
         return formatter()
