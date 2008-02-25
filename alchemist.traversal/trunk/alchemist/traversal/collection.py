95d6108184ce885d994212f1a33470c67f8f2fe0
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-02-25 17:10:10 +0000

    rework managed constraint traversal, introduce manager delegate to abstract constraint management

--- alchemist.traversal/trunk/alchemist/traversal/collection.py
+++ alchemist.traversal/trunk/alchemist/traversal/collection.py
@@ -4,6 +4,9 @@ $Id: $
 
 from zope import interface
 from zope.publisher.interfaces import NotFound
+from zope.location import ILocation
+from zope.security.proxy import removeSecurityProxy
+
 from z3c.traverser import interfaces
 
 class CollectionTraverserTemplate(object):
@@ -21,6 +24,10 @@ class CollectionTraverserTemplate(object):
         """See zope.publisher.interfaces.IPublishTraverse"""
         if name in self.collection_attributes:
             container = getattr( self.context, name )
+            if ILocation.providedBy( container ):
+                trusted_ctx = removeSecurityProxy( container )
+                trusted_ctx.__parent__ = self.context
+                trusted_ctx.__name__   = name
             return container
         raise NotFound( self.context, name, request)
         
--- alchemist.traversal/trunk/alchemist/traversal/managed.py
+++ alchemist.traversal/trunk/alchemist/traversal/managed.py
@@ -1,18 +1,22 @@
 """
+
 $Id: $
+
+functionally this gives us class descriptors that return alchemsit z3 containers 
+
+ideally we should hook into sa's property definition syntax, so we can have a z3 container
+as an sa instructmented container class.
 """
 
 from zope import interface
 from zope.dottedname.resolve import resolve
 from zope.app.security.protectclass import protectLikeUnto
 from zope.security.proxy import removeSecurityProxy
-
+from zope.location import ILocation
 from ore.alchemist.container import PartialContainer
 from sqlalchemy import orm
 
 import interfaces
-
-# alternatively hand turn a z3 container collection class for an object.
 
 class _ManagedContainer( PartialContainer ):
     
@@ -20,26 +24,94 @@ class _ManagedContainer( PartialContainer ):
         m = self.__class__.__bases__[1]
         s = "%s.%s"%(m.__module__, m.__name__ )
         return "<Managed %s>"%s
+
+    def __setitem__( self, key, value ):
+        super( _ManagedContainer, self ).__setitem__( key, value )
+        self.constraints.setConstrainedValues( self.__parent__, value )        
         
-class ManagedContainer(object):
+    def setConstraintManager( self, constraints ):
+        self.constraints = constraints
+        self.setQueryModifier( constraints.getQueryModifier( self.__parent__, self ) )
+
+class ConstraintManager( object ):
+    """
+    manages the constraints on a managed container
+    """
+    
+    def setConstrainedValues( self, instance, target ):
+        """
+        ensures existence of conformant constraint values
+        to match the query.
+        """
+        
+    def getQueryModifier( self, instance, container ):
+        """
+        given an instance inspect for the query to retrieve 
+        related objects from the given alchemist container.
+        """
+        
+class One2Many( ConstraintManager ):
+    
+    def __init__( self, fk):
+        self.fk = fk
+    
+    def getQueryModifier( self, instance, container ):
+        mapper = orm.class_mapper( instance.__class__ )
+        primary_key = mapper.primary_key_from_instance( instance )[0]
+        return container.domain_model.c[ self.fk ] == primary_key 
+        
+    def setConstrainedValues( self, instance, target ):
+        trusted = removeSecurityProxy( instance )
+        mapper = orm.object_mapper( trusted )
+        primary_key = mapper.primary_key_from_instance( trusted )[0]
+        column = target.__class__.c[ self.fk ]
+        setattr( target, column.name, primary_key )
+        
+class Many2Many( ConstraintManager ):
+
+    def __init__( self, join_fk ):
+        pass
+
+    def getQueryModifier( self, instance, container ):
+        mapper = orm.object_mapper( instance )
+        primary_key = mapper.primary_key_from_instance( instance )[0]
+        
+        #import pdb; pdb.set_trace()
+        # user_group_membership.group_id = groups.group_id 
+        # group
+        #container.domain_model.c[ self.fk ] ==
+        #sql.and_( 
+        #            )
+        return 
+        
+    def setConstrainedValues( self, instance, target ):
+        mapper = orm.object_mapper( instance )        
+        primary_key = mapper.primary_key_from_instance( instance )
+        setattr( target, column.name, primary_key )
+
+def one2many( name, container, fk ):
+    constraint = One2Many( fk )
+    container = ManagedContainerDescriptor( name, container, constraint )
+    return container
+    
+class ManagedContainerDescriptor(object):
     
     _container_class = None
     
     interface.implements( interfaces.IManagedContainer )
     
-    def __init__( self, name, container, fk):
+    def __init__( self, name, container, constraint):
         self.name = name
-        self.container = container
-        self.fk = fk
+        self.container  = container
+        self.constraint = constraint
                 
     def __get__( self, instance, class_):
         container = self.domain_container()
-        container.__parent__ = instance
-        container.__name__ = self.name
-        mapper = orm.object_mapper( instance )
-        primary_key = mapper.primary_key_from_instance( instance )[0]
-        container.setQueryModifier( container.domain_model.c[ self.fk ] == primary_key )
-        return removeSecurityProxy( container )
+        if ILocation.providedBy( instance ):
+            container.__parent__ = instance
+            container.__name__ = self.name
+        container.setConstraintManager( self.constraint )
+        return container
     
     @property
     def domain_container( self ):
@@ -49,3 +121,5 @@ class ManagedContainer(object):
         self._container_class = type( "ManagedContainer", ( _ManagedContainer, container_class), dict( container_class.__dict__) )        
         protectLikeUnto( self._container_class, container_class )
         return self._container_class
+
+    
