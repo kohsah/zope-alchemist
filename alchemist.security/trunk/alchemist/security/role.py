f3475a8665f95d6b6898bf44c9255c2b37aee595
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-04-16 04:26:22 +0000

    add local role support, and unit tests

--- alchemist.security/trunk/alchemist/security/role.py
+++ alchemist.security/trunk/alchemist/security/role.py
@@ -1,27 +1,31 @@
-from zope import interface, component
+"""
+
+Local And Global Principal Role Maps.
+
+These are somewhat naive implementations, for hybrid zodb/rdb systems, use of this implementation
+is not recommended, instead use alchemist keyreferences in conjunction with ore.annotation 
+(utility annotations).
+
+"""
+
+from zope import interface
 from zope.securitypolicy.interfaces import IPrincipalRoleMap 
 from zope.securitypolicy.interfaces import Allow, Deny, Unset
-import sqlalchemy as rdb
-import schema
 
-def get_parent_names( ob ):
-    names = []
-    while ob is not None:
-        names.append( ob.__name__ )
-        ob = ob.__parent__
-    return names
-    
-@interface.implementer( IPrincipalRoleMap )
-def adaptprm( object ):
-    prm = PrincipalRoleMap()
-    return prm
+from sqlalchemy import select, and_, orm
+from schema import prm
 
 BooleanAsSetting = { True : Allow, False : Deny, None : Unset }
 
-class PrincipalRoleMap( object ):
+class LocalPrincipalRoleMap( object ):
     
     interface.implements( IPrincipalRoleMap )
-
+    
+    def __init__( self, context ):
+        self.context = context
+        self.oid = orm.object_mapper( self.context ).primary_key_from_instance(self.context)[0]
+        self.object_type = context.__class__.__name__.lower()
+        
     def getPrincipalsForRole(self, role_id):
         """Get the principals that have been granted a role.
 
@@ -31,9 +35,13 @@ class PrincipalRoleMap( object ):
         If no principals have been assigned this role,
         then the empty list is returned.
         """
-        prm = schema.principal_role_map
-        s = rdb.select( [prm.c.principal_id, prm.c.setting] ).where( prm.c.role_id == role_id )  
-        return s.execute()
+        s = select( [prm.c.principal_id, prm.c.setting],
+                and_( prm.c.role_id == role_id,
+                          prm.c.object_type == self.object_type,
+                          prm.c.object_id == self.oid )
+                )
+        for o in s.execute():
+            yield o[0], BooleanAsSetting[ o[1] ]
 
     def getRolesForPrincipal(self, principal_id):
         """Get the roles granted to a principal.
@@ -44,20 +52,25 @@ class PrincipalRoleMap( object ):
         If no roles have been assigned to
         this principal, then the empty list is returned.
         """
-        prm = schema.principal_role_map
-        s = rdb.select( [prm.c.role_id, prm.c.setting] ).where( prm.c.principal_id == principal_id )
+        s = select( [prm.c.role_id, prm.c.setting],
+                and_( prm.c.principal_id == principal_id,
+                          prm.c.object_type == self.object_type,
+                          prm.c.object_id == self.oid )
+            )
         for o in s.execute():
             yield o[0], BooleanAsSetting[ o[1] ]
 
     def getSetting(self, role_id, principal_id):
         """Return the setting for this principal, role combination
         """
-        prm = schema.principal_role_map
-        s = rdb.select( [prm.c.settings] ).where( 
-                rdb.and_( prm.c.principal_id == principal_id, prm.c.role_id == role_id )
+        s = select( [prm.c.setting],
+                and_( prm.c.principal_id == principal_id,
+                          prm.c.role_id == role_id, 
+                          prm.c.object_type == self.object_type,
+                          prm.c.object_id == self.oid )                          
                 )
         results = s.execute()
-        return results.fetchone()[0]
+        return BooleanAsSetting[ results.fetchone()[0] ]
 
     def getPrincipalsAndRoles( self ):
         """Get all settings.
@@ -66,21 +79,42 @@ class PrincipalRoleMap( object ):
         setting for each combination as a sequence of tuples with the
         role id, principal id, and setting, in that order.
         """
-        prm = schema.principal_role_map
-        return prm.select( [prm.c.role_id, prm.c.principle_id, prm.c.setting ] ).execute()
+        s =select( [prm.c.role_id, prm.c.principal_id, prm.c.setting ],
+                    and_( prm.c.object_type == self.object_type,
+                          prm.c.object_id == self.oid )
+               )
+        for role_id, principal_id, setting in s.execute():
+            yield role_id, principal_id, BooleanAsSetting[ setting ]
         
     def assignRoleToPrincipal( self, role_id, principal_id ):
-        schema.principal_role_map.insert(
-            values=dict( role_id = role_id, principal_id = principal_id )
+        prm.insert(
+            values=dict( role_id = role_id, 
+                         principal_id = principal_id,
+                         object_id = self.oid,
+                         object_type = self.object_type )
             ).execute()
 
     def removeRoleFromPrincipal( self, role_id, principal_id ):
-        prm = schema.principal_role_map
         prm.delete(
-            rdb.and_( prm.c.role_id == role_id,
-                      prm.c.principal_id == principal_id )
+            and_( prm.c.role_id == role_id,
+                      prm.c.principal_id == principal_id,
+                      prm.c.object_type == self.object_type,
+                      prm.c.object_id == self.oid )                                                
             ).execute()
 
     def unsetRoleForPrincipal( self, role_id, principal_id ):
-        raise NotImplemented
+        prm.insert(
+            values=dict( role_id = role_id, 
+                         principal_id = principal_id,
+                         setting = False,
+                         object_id = self.oid,
+                         object_type = self.object_type )
+            ).execute()        
+
     
+class GlobalPrincipalRoleMap( LocalPrincipalRoleMap ):
+    
+    def __init__( self, context ):
+        self.context = context
+        self.object_type = None
+        self.oid = None
--- alchemist.security/trunk/alchemist/security/schema.py
+++ alchemist.security/trunk/alchemist/security/schema.py
@@ -2,45 +2,35 @@ import sqlalchemy as rdb
 
 metadata = rdb.MetaData()
 
-permissions = rdb.Table(
-   "zope_permissions",
-   metadata,
-   rdb.Column( "permission_id", rdb.Integer, primary_key=True ),
-   rdb.Column( "name", rdb.Unicode(30), unique=True  ),
-   )
-
-roles = rdb.Table(
-   "zope_roles",
-   metadata,
-   rdb.Column( "role_id", rdb.Integer, primary_key=True ),
-   rdb.Column( "name", rdb.Unicode(30), primary_key=True ),   
-   )
-
-permission_role_map = rdb.Table(
+role_permission_map = rdb.Table(
    "zope_role_permission_map",
    metadata,
    rdb.Column( "role_id", rdb.Unicode(50) ),
    rdb.Column( "permission_id", rdb.Unicode(50) ),
-   rdb.Column( "object_type", rdb.Unicode(50), ),
-   rdb.Column( "object_id", rdb.Unicode(40),  ),   
+   rdb.Column( "object_type", rdb.Unicode(100), ),
+   rdb.Column( "object_id", rdb.Integer ),   
    )
+   
+rpm = role_permission_map   
+
+rdb.Index( "rpm_oid_idx", 
+            role_permission_map.c['object_id'],            
+            role_permission_map.c['object_type'] )
    
 principal_role_map = rdb.Table(
    "zope_principal_role_map",
    metadata,
-#   rdb.Column( "principal_id", rdb.Integer, index=True, nullable=False ),
    rdb.Column( "principal_id", rdb.Unicode(50), index=True, nullable=False ),#    # 
    rdb.Column( "role_id", rdb.Unicode(50), nullable=False ),   
    rdb.Column( "setting", rdb.Boolean, default=True, nullable=False ),
-#   rdb.Column( "object_type", rdb.Un, ),      
-#   rdb.Column( "object_id", rdb.Integer, ),         
+   rdb.Column( "object_type", rdb.Unicode(100) ),      
+   rdb.Column( "object_id", rdb.Integer ),         
    )
+   
+prm = principal_role_map   
+   
+rdb.Index( "prm_oid_idx",
+            principal_role_map.c['object_id'],            
+            principal_role_map.c['object_type'] )
   
-def main( ):
-    db = rdb.create_engine('postgres://localhost/bungeni')
-    metadata.bind = db
-    metadata.create_all()
-    
-if __name__ == '__main__':
-    main()
 
