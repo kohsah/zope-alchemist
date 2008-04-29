bdd8818f923a6e25c1a0ef5a8d7f26568909ca65
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-04-29 05:31:21 +0000

    pin sqlalchemy version for now to 0.4.4

--- alchemist.catalyst/trunk/setup.py
+++ alchemist.catalyst/trunk/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="alchemist.catalyst",
     version="0.4.0dev",
-    install_requires=['setuptools', 'ore.alchemist', 'alchemist.ui', 'alchemist.traversal'],
+    install_requires=['setuptools', 'ore.alchemist', 'alchemist.ui', 'alchemist.traversal', "SQLAlchemy==0.4.4"],
     packages=find_packages(exclude=["*.tests"]),
     #package_dir= {'':'src'},
     namespace_packages=['alchemist'],
