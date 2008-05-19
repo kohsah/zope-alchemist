69cd68cc2802966dcc2adf1e00f365718a584210
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-05-19 16:43:11 +0000

    update description

--- alchemist.ui/trunk/setup.py
+++ alchemist.ui/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="alchemist.ui",
-    version="0.3.1",
+    version="0.3.2",
     install_requires=['setuptools', 'ore.alchemist', 'zope.formlib', 'zc.table', 'zope.viewlet'],
     packages=find_packages(exclude=["*.tests"]),
     namespace_packages=['alchemist'],
@@ -12,10 +12,7 @@ setup(
     zip_safe=False,
     author='Kapil Thangavelu',
     author_email='kapil.foss@gmail.com',
-    description="""\
-alchemist.ui contains generic user interface components for use
-with achemist.core based systems.
-""",
+    description="user interface components for use for zope3 database applications, crud and relation views",
     license='LGPL',
     keywords="zope zope3",
     )
