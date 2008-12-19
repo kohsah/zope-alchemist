d7e59a82ce11c8f52b9a2ce1b49e0c97847c44c0
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-12-19 02:50:38 +0000

    version bump -> 0.4.1, and release

--- alchemist.ui/trunk/setup.py
+++ alchemist.ui/trunk/setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
 
 setup(
     name="alchemist.ui",
-    version="0.4.0",
+    version="0.4.1",
     install_requires=['setuptools',
                       'ore.alchemist',
                       'zope.formlib',
