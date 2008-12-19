2b92f649dcc185bd4328d3b26f66dc1ea17f4bb6
Kapil Thangavelu <kapilt@users.noreply.github.com>
2008-12-19 02:49:13 +0000

    version bump -> 0.4.0, and release

--- alchemist.ui/trunk/setup.py
+++ alchemist.ui/trunk/setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
     name="alchemist.ui",
@@ -17,7 +21,11 @@ setup(
     zip_safe=False,
     author='Kapil Thangavelu',
     author_email='kapil.foss@gmail.com',
-    description="user interface for use with zope3 database applications, crud, listing and relation views",
+    description="user interface components for use with zope3 relational database applications, crud, listing and relation views",
+    long_description = read('changes.txt'),
     license='LGPL',
+    classifiers=['Programming Language :: Python',
+                 'Framework :: Zope3',
+                 ],        
     keywords="zope zope3",
     )
